2ce99dd2fcb113fd6bad741756a2b20c29183754
buehara <buehara@student.42sp.org.br>
2025-12-27 20:03:12 -0300

    update: functions + libft.h

--- src/main.c
+++ src/main.c
@@ -6,11 +6,11 @@
 /*   By: buehara <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/24 22:15:09 by buehara           #+#    #+#             */
-/*   Updated: 2025/12/27 11:58:52 by buehara          ###   ########.fr       */
+/*   Updated: 2025/12/27 18:45:18 by buehara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "42_Libft/includes/libft.h"
+#include "../pipex.h"
 
 char	**path_parsing(char **envp)
 {
@@ -49,41 +49,73 @@ void	free_path(char **split)
 	free(split);
 }
 
-void	error_exit(void)
+void	error_free(char	**str, int fd1, int fd2)
 {
+	if (str)
+		free_path(str);
+	if (fd1 != NO_FD)
+		close(fd1);
+	if (fd2 != NO_FD)
+		close(fd2);
 	perror("Error");
 	exit(ERROR);
 }
 
-void	error_free(char	*str)
-{
-	free_path(str);
-	error_exit();
-}
-
-void	error_fd(int fd)
-{
-	close(fd);
-	error_exit();
-}
-
 void	pipex_init(int argc, char **argv, int *infile, int *outfile)
 {
+	int	file_permissions;
+
 	if (argc < 3)
 	{
 		ft_putstr_fd("Error: Insufficient parameters", 2);
-		return (ERROR);
+		exit (ERROR);
 	}
 	*infile = open(argv[1], O_RDONLY);
 	if (*infile == -1)
-		error_exit();
+		error_free(NULL, NO_FD, NO_FD);
 	if (!ft_strncmp(argv[1], "here_doc", 9))
 		file_permissions = O_CREAT | O_WRONLY | O_APPEND;
 	else
 		file_permissions = O_CREAT | O_WRONLY | O_TRUNC;
-	*onfile = open(argv[argc - 1], file_permissions, 0644);
-	if (*onfile == -1)
-		error_fd(*infile);
+	*outfile = open(argv[argc - 1], file_permissions, 0644);
+	if (*outfile == -1)
+		error_free(NULL, *infile, NO_FD);
+}
+
+void	arg_check(int argc, char **argv, int infile, int outfile)
+{
+	int	idx;
+
+	idx = 1;
+	while (idx < argc - 1)
+	{
+		if (argv[idx][0] == '\0')
+		{
+			ft_putstr_fd("Error: Empty Argument given\n", STD_ERR);
+			close(infile);
+			close(outfile);
+			exit (ERROR);
+		}
+		idx++;
+	}
+}
+
+char	**arg_parse(char **envp)
+{
+	int		idx;
+	char	**path_split;
+	char	**path_join;
+	
+	path_split = path_parsing(envp); // TEST OK
+	idx = 0;
+	path_join = NULL;
+	while (path_split[idx] != NULL)
+	{
+		path_join[idx] = ft_strjoin(path_split[idx], "/");
+		idx++;
+	}
+	free_path(path_split);
+	return (path_join);
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -91,15 +123,17 @@ int	main(int argc, char **argv, char **envp)
 	char	*path;
 	int		infile;
 	int		outfile;
-	char	**path_split;
+	//char	**path_split;
 	char	**path_join;
-	int		fd[2];
-	pid_t	pid;
-	int		index;
+	//int		fd[2];
+	//pid_t	pid;
+	//int		index;
 	int		idx;
-	int		file_permissions;
+	//int		file_permissions;
 
-	if (argc < 3)
+	path = NULL;
+	pipex_init(argc, argv, &infile, &outfile); // TEST PENDING
+/*	if (argc < 3)
 	{
 		ft_putstr_fd("Error: Insufficient parameters", 2);
 		return (ERROR);
@@ -113,18 +147,24 @@ int	main(int argc, char **argv, char **envp)
 		file_permissions = O_CREAT | O_WRONLY | O_TRUNC;
 	onfile = open(argv[argc - 1], file_permissions, 0644);
 	if (onfile == -1)
-		error_fd(infile);
-	idx = 1;
+		error_fd(infile);*/
+
+	arg_check(argc, argv, infile, outfile); // TEST PENDING
+	/*idx = 1;
 	while (idx < argc - 1)
 	{
 		if (argv[idx][0] == '\0')
 		{
 			ft_putstr_fd("Error: Empty Argument given\n", STD_ERR);
-			return (ERROR);
+			close(infile);
+			close(outfile);
+			exit (ERROR);
 		}
 		idx++;
-	}
-	path_split = path_parsing(envp);
+	}*/
+
+	path_join = arg_parse(envp);
+	/*path_split = path_parsing(envp); // TEST OK
 	idx = 0;
 	path_join = NULL;
 	while (path_split[idx] != NULL)
@@ -132,29 +172,36 @@ int	main(int argc, char **argv, char **envp)
 		path_join[idx] = ft_strjoin(path_split[idx], "/");
 		idx++;
 	}
-	free_path(path_split);
+	free_path(path_split);*/
+	idx = 0;
+	while (path_join[idx] != NULL)
+		ft_printf("->\t%s", path_join[idx++]);
+	/*path = NULL;
 	if (pipe(fd) == -1)
-		error_message(path_join);
+		error_free(path_join, NO_FD, NO_FD);
 	pid = fork();
 	if (pid == -1)
-		error_message(path_join);
-	if (pid = 0)
+		error_free(path_join);
+	idx = 0;
+	index = 0;
+	if (pid == 0)
 	{
-		close(fd[0]);
+		close(fd[fd_close]);
 		path = ft_strjoin(path_join[idx], argv[index]);
-		while (access(path, X_OR) == -1)
+		while (access(path, X_OK) == -1)
 		{
 			free(path);
 			path = ft_strjoin(path_join[idx], argv[index]);
 		}
-		if (execve(path, argv[index], envp) == -1)
+		if (execve(path, &argv[index], envp) == -1)
 		{
 			free(path);
 			perror("Error");
 			return (ERROR);
 		}
-	}
-	free(path);
+	}*/
+	if (path)
+		free(path);
 	free_path(path_join);
 	return (SUCCESS);
 }
