ff5cfec18b4a0e689a5e1b1744f728464ed8c22f
buehara <buehara@student.42sp.org.br>
2025-12-29 15:09:52 -0300

    organizing functions + correting cmd splits inside children

--- src/main.c
+++ src/main.c
@@ -6,61 +6,11 @@
 /*   By: buehara <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/24 22:15:09 by buehara           #+#    #+#             */
-/*   Updated: 2025/12/28 20:55:32 by buehara          ###   ########.fr       */
+/*   Updated: 2025/12/29 14:40:54 by buehara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex.h"
-
-char	**path_parsing(char **envp)
-{
-	int		index;
-	char	*temp;
-	char	*env_path;
-	char	**path_split;
-
-	index = 0;
-	env_path = NULL;
-	while (envp[index] != NULL)
-	{
-		if (!ft_strncmp(envp[index], "PATH=", 5))
-		{
-			env_path = envp[index];
-			break ;
-		}
-		index++;
-	}
-	path_split = ft_split(env_path, ':');
-	temp = path_split[0];
-	path_split[0] = ft_strdup(ft_strchr(temp, '/'));
-	free(temp);
-	return (path_split);
-}
-
-void	free_path(char **split)
-{
-	int	index;
-
-	index = 0;
-	while (split[index] != NULL)
-	{
-		free(split[index]);
-		index++;
-	}
-	free(split);
-}
-
-void	error_free(char	**str, int fd1, int fd2)
-{
-	if (str)
-		free_path(str);
-	if (fd1 != NO_FD)
-		close(fd1);
-	if (fd2 != NO_FD)
-		close(fd2);
-	perror("Error");
-	exit(ERROR);
-}
 
 void	pipex_init(int argc, char **argv, int *infile, int *outfile)
 {
@@ -83,51 +33,6 @@ void	pipex_init(int argc, char **argv, int *infile, int *outfile)
 		error_free(NULL, NO_FD, NO_FD);
 }
 
-void	arg_check(int argc, char **argv)
-{
-	int	idx;
-
-	idx = 1;
-	while (idx < argc)
-	{
-		if (argv[idx][0] == '\0')
-		{
-			ft_putstr_fd("Error: Empty Argument given\n", STD_ERR);
-			exit (ERROR);
-		}
-		idx++;
-	}
-}
-
-char	**arg_parse(char **envp)
-{
-	int		idx;
-	char	**path_split;
-	char	**path_join;
-	
-	path_split = path_parsing(envp);
-	idx = 0;
-	path_join = NULL;
-	while (path_split[idx] != NULL)
-		idx++;
-	path_join = ft_calloc(++idx, sizeof(char *));
-	idx = 0;
-	while (path_split[idx] != NULL)
-	{
-		path_join[idx] = ft_strjoin(path_split[idx], "/");
-		idx++;
-	}
-	path_join[idx] = NULL;
-	free_path(path_split);
-	return (path_join);
-}
-
-typedef struct	s_cmd
-{
-	char	**cmd1;
-	char	**cmd2;
-}				t_cmd;
-
 int	main(int argc, char **argv, char **envp)
 {
 	char	*path;
@@ -149,8 +54,6 @@ int	main(int argc, char **argv, char **envp)
 //	while (path_join[idx] != NULL)
 //		ft_printf("->\t%s\n", path_join[idx++]);
 
-	cmd.cmd1 = ft_split(argv[2], ' ');
-	cmd.cmd2 = ft_split(argv[3], ' ');
 	path = NULL;
 	if (pipe(fd) == -1)
 		error_free(path_join, infile, outfile);
@@ -175,6 +78,9 @@ int	main(int argc, char **argv, char **envp)
 		close(outfile);
 		close(infile);
 		close(fd[1]);
+		cmd.cmd1 = ft_split(argv[2], ' ');
+		if (!cmd.cmd1)
+			exit (ERROR);
 		if (access(cmd.cmd1[0], X_OK) == -1)
 		{
 			path = ft_strjoin(path_join[idx], cmd.cmd1[0]);
@@ -193,7 +99,6 @@ int	main(int argc, char **argv, char **envp)
 		}
 		else
 			path = cmd.cmd1[0];
-		free_path(cmd.cmd2);
 		if (execve(path, cmd.cmd1, envp) == -1)
 		{
 			free(path);
@@ -216,6 +121,9 @@ int	main(int argc, char **argv, char **envp)
 		close(infile);
 		close(fd[0]);
 		close(outfile);
+		cmd.cmd2 = ft_split(argv[3], ' ');
+		if (!cmd.cmd2)
+			exit (ERROR);
 		if (access(cmd.cmd2[0], X_OK) == -1)
 		{
 			path = ft_strjoin(path_join[idx], cmd.cmd2[0]);
@@ -234,7 +142,6 @@ int	main(int argc, char **argv, char **envp)
 		}
 		else
 			path = cmd.cmd2[0];
-		free_path(cmd.cmd1);
 		if (execve(path, cmd.cmd2, envp) == -1)
 		{
 			free(path);
