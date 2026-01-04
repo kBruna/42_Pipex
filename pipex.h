290594c133f92b267aa32ff28facacf2d8b22d12
buehara <buehara@student.42sp.org.br>
2026-01-04 20:08:00 -0300

    fix: add function prototype and organization

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: buehara <buehara@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 15:26:39 by buehara           #+#    #+#             */
-/*   Updated: 2026/01/04 15:26:41 by buehara          ###   ########.fr       */
+/*   Updated: 2026/01/04 18:36:21 by buehara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,16 @@ typedef struct	s_cmd
 char	**path_parsing(char **envp);
 char	**arg_parse(char **envp);
 void	arg_check(int argc, char **argv);
+void	pipex_init(int argc, char **argv, int *infile, int *outfile);
 
 // ----------- Pipex_utils.c ---------
 void	free_path(char **split);
 void	error_free(char	**str, int fd1, int fd2);
 
+// ----------- Pipex.c ---------------
+void	pipe_dup(int fd, int infile, int outfile, char **path_join);
+
+
 // ----------- Main.c ----------------
-void	pipex_init(int argc, char **argv, int *infile, int *outfile);
 
 #endif
